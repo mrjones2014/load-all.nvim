3011fe2075563627a818bf99301e82af07cd1a90
Mat Jones <mat.jones@agilebits.com>
2021-08-13 10:20:33 -0400

    check if file is a lua file before doing dofile

--- lua/load-all/init.lua
+++ lua/load-all/init.lua
@@ -1,7 +1,15 @@
+local fileExtension = '.lua'
+
+local function isLuaFile(filename)
+  return filename:sub(-#fileExtension) == fileExtension
+end
+
 local function loadAll(path)
   local scan = require('plenary.scandir')
   for _, file in ipairs(scan.scan_dir(path, { depth = 0 })) do
-    dofile(file)
+    if isLuaFile(file) then
+      dofile(file)
+    end
   end
 end
 

9a6104588b5fc81907fe5e267cdd817851bdb266
Mat Jones <mat.jones@agilebits.com>
2021-08-13 10:05:04 -0400

    format with stylua

--- lua/load-all/init.lua
+++ lua/load-all/init.lua
@@ -1,8 +1,8 @@
 function loadAll(path)
-	local scan = require("plenary.scandir")
-	for _, file in ipairs(scan.scan_dir(path, { depth = 0 })) do
-		dofile(file)
-	end
+  local scan = require('plenary.scandir')
+  for _, file in ipairs(scan.scan_dir(path, { depth = 0 })) do
+    dofile(file)
+  end
 end
 
 return loadAll
